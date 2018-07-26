e85bddb27f703f589122d833e97eafa629559ff7
grayswand1r <josephhane@gmail.com>
2018-07-26 15:50:35 -0400

    Completed Tour of Heroes Pt 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 
+import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
+
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent
   ],
   imports: [
-    BrowserModule
-  ],
+  BrowserModule,
+  FormsModule
+],
   providers: [],
   bootstrap: [AppComponent]
 })
